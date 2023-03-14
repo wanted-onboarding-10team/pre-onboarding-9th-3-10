721cde9650968f0801151dc9ab4ff0e45b907e1d
yoosion030 <rsy5487@naver.com>
2023-03-14 20:45:59 +0900

    [#3] feature: custom tooltip 제작
    요구사항에 맞게 hover 시 id, value_area, value_bar를 제공

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,18 +1,41 @@
-import { Area, Bar, CartesianGrid, ComposedChart, Legend, Tooltip, XAxis, YAxis } from 'recharts';
+import {
+  Area,
+  Bar,
+  CartesianGrid,
+  ComposedChart,
+  Legend,
+  Tooltip,
+  TooltipProps,
+  XAxis,
+  YAxis,
+} from 'recharts';
 import { ChartDataType } from 'types/data';
 
 interface ChartProps {
   data?: ChartDataType[];
 }
-
 const Chart = ({ data }: ChartProps) => {
+  const CustomTooltip = ({ active, payload }: TooltipProps<number, string>) => {
+    if (active && payload) {
+      return (
+        <div className='custom-tooltip'>
+          <div>{`ID: ${payload[0].payload.id}`}</div>
+          <div>{`value_bar: ${payload[0].value}`}</div>
+          <div>{`value_area: ${payload[1].value}`}</div>
+        </div>
+      );
+    }
+    return null;
+  };
+
   return data ? (
     <ComposedChart width={1400} height={800} data={data}>
       <CartesianGrid strokeDasharray='3 3' />
       <XAxis dataKey='time' />
       <YAxis dataKey='value_bar' tickCount={8} />
       <YAxis dataKey='value_bar' tickCount={8} />
-      <Tooltip />
+      <Tooltip content={<CustomTooltip />} contentStyle={{ backgroundColor: '#fff' }} />
+
       <Legend />
 
       <Bar type='monotone' dataKey='value_bar' fill='#82ca9d' />
