e3ca5e0ae59810c9239ff47355159e35ccddf58e
hongsoom <hongsoom@kakao.com>
2023-03-14 17:00:14 +0900

    [#3] style : tootip의 text, layout 작성
    id는 h3, 다른 text들은 p태그로 작성

---
+++ src/styles/Layout.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const CategoryDiv = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const TooltopDiv = styled.div`
+  background-color: white;
+  border: 1px solid #fffafa;
+  padding: 5px;
+`;
---
+++ src/styles/Text.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Title = styled.h3`
+  margin: 0;
+`;
+
+export const Item = styled.p`
+  margin-bottom: 0;
+`;
