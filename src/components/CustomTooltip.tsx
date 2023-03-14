15ed8755912a0abf73747cb6bf25ea49e0ce2269
DESKTOP-1V9OQ1L\GIGABYTE <emma05127@gmail.com>
2023-03-14 21:13:37 +0900

    [#2] fix: import 오류 수정
    Text 컴포넌트를 recharts가 아닌 chakra-ui에서 가저오도록 수정

--- src/components/CustomTooltip.tsx
+++ src/components/CustomTooltip.tsx
@@ -1,14 +1,15 @@
-import { Box, Flex } from '@chakra-ui/react';
-import { css } from '@emotion/react';
-import { Text, TooltipProps } from 'recharts';
+import { Flex, Text } from '@chakra-ui/react';
+import { TooltipProps } from 'recharts';
 import { NameType, ValueType } from 'recharts/types/component/DefaultTooltipContent';
 import img from 'assets/tooltipBox.png';
 import styled from '@emotion/styled';
+
 const CustomTooltip = ({ active, payload, label }: TooltipProps<ValueType, NameType>) => {
   if (active && payload && payload.length) {
     const {
       payload: { id, value_area, value_bar },
     } = payload[0];
+
     return (
       <Content>
         <Flex direction={'column'}>
