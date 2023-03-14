10b4b22d65bac02a4f6221958a7a1d6253dc078d
hongsoom <hongsoom@kakao.com>
2023-03-14 14:00:45 +0900

    [#2] feature : useChart hooks을 사용하여 응답받은 data 가공
    id - new Set()을 사용하여 중복된 data를 제거함
    day - x축에 시:분 형식으로 출력하기 위해 day.js을 사용함
    value_day - 데이터를 클릭할 때 하이라이트 기능 구현하기 위해 필요한
    property 추가
    이 모든 걸  객체를 배열로 저장함

---
+++ src/hooks/useChart.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useMemo, useState } from 'react';
+import dayjs from 'dayjs';
+import { getChart } from '../api/api';
+import type { Chart, ChartResponse } from '../types/Chart';
+
+const useChart = () => {
+  const [chartData, setChartData] = useState<Chart[]>([]);
+
+  const locations = useMemo(() => {
+    if (!chartData) return [];
+    return [...new Set(chartData.map(data => data.id))];
+  }, [chartData]);
+
+  useEffect(() => {
+    (async () => {
+      const data = await getChart();
+      const newData = Object.entries(data as ChartResponse)?.map(
+        ([day, { id, value_area, value_bar }]) => ({
+          day: dayjs(day).format('HH:mm'),
+          id,
+          value_area,
+          value_bar,
+          value_day: day,
+        }),
+      );
+      setChartData(newData);
+    })();
+  }, []);
+  return { data: chartData, locations: locations };
+};
+
+export default useChart;
