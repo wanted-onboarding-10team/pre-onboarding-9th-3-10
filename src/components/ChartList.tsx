726f7141998117e6ce9b6ebd4a2489fcdfa19798
hongsoom <hongsoom@kakao.com>
2023-03-14 15:56:50 +0900

    [#2] feature : 시계열 차트 만들기 기능 구현
    recharts 라이브러리를 사용하여 만듬

---
+++ src/components/ChartList.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import {
+  Area,
+  Bar,
+  CartesianGrid,
+  Cell,
+  ComposedChart,
+  Legend,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from 'recharts';
+import { Chart } from '../types/Chart';
+import Category from './Category';
+
+interface ChartProps {
+  data: Chart[];
+  location: string;
+  changeLocation: (value: string) => void;
+}
+
+const ChartList = ({ data, location, changeLocation }: ChartProps) => {
+  const [category, setCategory] = useState<string>('TOTAL');
+
+  return (
+    <>
+      <Category setCategory={setCategory} />
+      <ResponsiveContainer width='90%' height={500}>
+        <ComposedChart data={data} margin={{ top: 40, right: 40, bottom: 30, left: 40 }}>
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='time' />
+          <YAxis
+            dataKey='value_area'
+            yAxisId='left'
+            orientation='left'
+            label={{
+              value: 'Area',
+              angle: -90,
+              position: 'insideLeft',
+            }}
+          />
+          <YAxis
+            dataKey='value_bar'
+            yAxisId='right'
+            orientation='right'
+            label={{
+              value: 'Bar',
+              angle: 90,
+              position: 'insideRight',
+            }}
+          />
+          {(category === 'TOTAL' || category === 'AREA') && (
+            <Area
+              yAxisId='left'
+              type='monotone'
+              dataKey='value_area'
+              stroke='#C0C0C0'
+              fill='#C0C0C0'
+            ></Area>
+          )}
+
+          {(category === 'TOTAL' || category === 'BAR') && (
+            <Bar
+              yAxisId='right'
+              data={data}
+              dataKey='value_bar'
+              fill='#FFE4E1'
+              onClick={value => changeLocation(value.id)}
+            >
+              {data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={entry.id === location ? '#FFB6C1' : '#FFE4E1'} />
+              ))}
+            </Bar>
+          )}
+          <Legend />
+        </ComposedChart>
+      </ResponsiveContainer>
+    </>
+  );
+};
+
+export default ChartList;
