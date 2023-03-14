196d59dc85451d8768facc66551f8224d3d9af71
hongsoom <hongsoom@kakao.com>
2023-03-14 16:00:08 +0900

    [#2] style : button, layout들의 스타일 작성
    사용하는 해당 파일 안에다 적어도 되지만 가독성과  button과 layout의
    관련된 기능을 더 구현할 수 있기 때문에 따로 빼서 작성

---
+++ src/styles/button.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const CategoryBtn = styled.button`
+  width: 50px;
+  height: 30px;
+  margin-right: 10px;
+  border: solid #000 1px;
+  background: #000;
+  color: #fff;
+  cursor: pointer;
+`;
