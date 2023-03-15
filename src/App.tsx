9587b48d7485e3f2d07227f1c9d8c938fddd6638
yoosion030 <rsy5487@naver.com>
2023-03-15 17:16:44 +0900

    [#4] feature: 필터링 기능 구현
    체크박스를 누를 시 url을 변경하여 그 값을 받아와 해당 값에 맞게 필터링 하게 하였습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,32 @@ import { Chart, MainLayout } from 'components';
 import { useEffect, useState } from 'react';
 import { ChartDataType, MockDataType } from 'types/data';
 import { getMockDataApi } from 'utils/api/chart';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
-  const [mockData, setMockData] = useState<MockDataType>();
+  const navigate = useNavigate();
+
+  const [mockData, setMockData] = useState<ChartDataType[]>();
   const [chartData, setChartData] = useState<ChartDataType[]>();
 
+  const filteringData = mockData?.map(item => item.id) ?? [];
+  const category = filteringData.filter((item, index) => filteringData.indexOf(item) === index);
+
+  const [test, setTest] = useState<string | null>();
+  const searchParams = new URLSearchParams(location.search);
+
   const getMockData = async () => {
-    const data = await getMockDataApi();
-    setMockData(data);
+    const data: MockDataType = await getMockDataApi();
+    const dataArray: ChartDataType[] = Object.entries(data).map(
+      ([time, { id, value_area, value_bar }]) => ({
+        time,
+        id,
+        value_area,
+        value_bar,
+      }),
+    );
+    setMockData(dataArray);
+    setChartData(dataArray);
   };
 
   useEffect(() => {
@@ -17,21 +35,35 @@ function App() {
   }, []);
 
   useEffect(() => {
-    if (mockData) {
-      const data: ChartDataType[] = Object.entries(mockData).map(
-        ([time, { id, value_area, value_bar }]) => ({
-          time,
-          id,
-          value_area,
-          value_bar,
-        }),
-      );
-      setChartData(data);
+    test
+      ? setChartData(mockData?.filter(v => v.id === category[Number(test)]))
+      : setChartData(mockData);
+  }, [test]);
+
+  useEffect(() => {
+    setTest(searchParams.get('id'));
+  }, [searchParams]);
+
+  const handleFilter = (e: any, id: number) => {
+    if (e.target.checked) navigate(`/items?id=${id}`);
+    else {
+      navigate(`/`);
     }
-  }, [mockData]);
+  };
 
   return (
     <MainLayout>
+      {category.map((id, idx) => (
+        <div key={idx}>
+          {id}
+          <input
+            type='checkbox'
+            value={idx}
+            onChange={e => handleFilter(e, idx)}
+            checked={test !== null && idx === Number(test)}
+          />
+        </div>
+      ))}
       <Chart data={chartData} />
     </MainLayout>
   );
