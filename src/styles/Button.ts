bb1ec1df6ffa66368a6f70af5ae6f3d4aea72a64
hongsoom <hongsoom@kakao.com>
2023-03-15 13:58:09 +0900

    [#4] style : 필터 버튼 색상과 전체 화면 레이아웃 설정

--- src/styles/Button.ts
+++ src/styles/Button.ts
@@ -9,3 +9,16 @@ export const CategoryBtn = styled.button`
   color: #fff;
   cursor: pointer;
 `;
+
+export const FilterBtn = styled.button`
+  width: 100px;
+  padding: 0 20px;
+  height: 30px;
+  margin-right: 10px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  background: rgba(0, 0, 0, 0.1);
+  color: #696969;
+  cursor: pointer;
+  border-radius: 20px;
+  font-family: 'NotoSansB';
+`;
--- src/styles/Layout.ts
+++ src/styles/Layout.ts
@@ -2,11 +2,21 @@ import styled from 'styled-components';
 
 export const CategoryDiv = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: flex-end;
+`;
+
+export const ChartDiv = styled.div`
+  margin: 0 auto;
 `;
 
 export const TooltopDiv = styled.div`
   background-color: white;
   border: 1px solid #fffafa;
   padding: 5px;
+`;
+
+export const FilterDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10px;
 `;
