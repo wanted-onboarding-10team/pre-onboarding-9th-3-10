be622040ef0814ce6e30b746ca2f814bab778723
Ryong-E <think8867@gmail.com>
2023-03-15 18:18:48 +0900

    [#4] refactor: 사용하지 않는 파라미터 삭제

--- src/components/graph/tooltip/CutsomToolip.tsx
+++ src/components/graph/tooltip/CutsomToolip.tsx
@@ -2,7 +2,7 @@ import { TooltipProps } from 'recharts';
 import { NameType, ValueType } from 'recharts/types/component/DefaultTooltipContent';
 import styled from 'styled-components';
 
-const CustomToolip = ({ active, payload, label }: TooltipProps<ValueType, NameType>) => {
+const CustomToolip = ({ active, payload }: TooltipProps<ValueType, NameType>) => {
   if (active && payload && payload.length) {
     const { id, value_area, value_bar } = payload[0].payload;
     return (
