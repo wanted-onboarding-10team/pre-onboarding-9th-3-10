4affd47042859b00c2520f9356aa97725167dec5
Ryong-E <think8867@gmail.com>
2023-03-15 18:19:30 +0900

    [#4] feature: 특정 그래프 클릭시 하이라이트 기능 구현
    recharts에서 지원하는 onClick 이벤트를 통해 클릭된 값을 가져온뒤 query parameter로 지역이름 추가
    동일한 값이 query parameter에 있으면 삭제
    query parameter에 값이 있으면 동일한 값을 가진 차트는 모두 하이라이트 처리
    클릭 이벤트 타입은 rechart에서 타입을 any[]로 넘겨주어 any로 설정

--- src/components/graph/index.tsx
+++ src/components/graph/index.tsx
@@ -1,7 +1,11 @@
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import {
   Area,
   Bar,
+  Brush,
   CartesianGrid,
+  Cell,
   ComposedChart,
   Legend,
   ResponsiveContainer,
@@ -10,20 +14,95 @@ import {
   YAxis,
 } from 'recharts';
 import { MockDataType } from 'types';
-import CustomToolip from './tooltip/CutsomToolip';
+import CustomToolip from 'components/graph/tooltip/CutsomToolip';
 
 const Graph = ({ data }: { data: MockDataType[] }) => {
+  const [highlightList, setHightlightList] = useState<string[]>([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleClick = (e: any) => {
+    const {
+      payload: { id }, // 클릭한 지역이름
+    } = e.activePayload[0];
+
+    const area = searchParams.get('area')?.split(',');
+    //값이 없을 떄
+    if (area === undefined) return setSearchParams({ area: id });
+    //동일한 값 있을 때 삭제
+    if (area.some(area => area === id)) {
+      const exceptionArea = area.filter(area => area !== id);
+      if (exceptionArea.length === 0) return setSearchParams();
+      return setSearchParams({ area: exceptionArea.join(',') });
+    }
+    // 지역 추가
+    setSearchParams({ area: area.join(',') + ',' + id });
+  };
+
+  useEffect(() => {
+    const area = searchParams.get('area')?.split(',');
+    if (area === undefined) return setHightlightList([]);
+    setHightlightList([...area]);
+  }, [searchParams]);
+
   return (
     <ResponsiveContainer width='100%' height='100%'>
-      <ComposedChart data={data} barCategoryGap='40%'>
+      <ComposedChart data={data} onClick={handleClick}>
         <CartesianGrid strokeDasharray='3 3' />
         <Legend />
         <Tooltip content={<CustomToolip />} />
         <XAxis dataKey='name' tickSize={15} />
-        <YAxis dataKey='value_area' orientation='right' yAxisId={0} />
-        <YAxis dataKey='value_bar' orientation='left' yAxisId={1} />
-        <Area type='monotone' dataKey='value_area' stroke='#8884d8' fill='#8884d8' yAxisId={0} />
-        <Bar dataKey='value_bar' maxBarSize={10} fill='#413ea0' yAxisId={1} />
+        <Brush dataKey={'name'} height={30} />
+        <YAxis
+          dataKey='value_area'
+          orientation='right'
+          yAxisId={0}
+          label={{ value: 'value_area', angle: 90, position: 'insideRight' }}
+        />
+        <YAxis
+          dataKey='value_bar'
+          orientation='left'
+          yAxisId={1}
+          name='value_bar'
+          label={{ value: 'value_bar', angle: -90, position: 'insideLeft' }}
+        />
+        <Bar type='monotone' dataKey='value_bar' yAxisId={1} fill={'#5388D899'}>
+          {data.map((entry, index) => {
+            return (
+              <Cell
+                key={`cell-${index}`}
+                strokeWidth={highlightList.some(element => element === entry.id) ? 4 : 2}
+                fill={
+                  highlightList.some(element => element === entry.id)
+                    ? '#F4BE37'
+                    : 'rgba(44,44,44,0.3)'
+                }
+              />
+            );
+          })}
+        </Bar>
+        <Area
+          type='monotone'
+          dataKey='value_area'
+          stroke='rgb(29,195,187)'
+          fill='rgb(248,78,139)'
+          strokeWidth={3}
+          yAxisId={0}
+        >
+          {data.map((entry, index) => {
+            return (
+              <Cell
+                key={`cell-${index}`}
+                strokeWidth={highlightList.some(element => element === entry.id) ? 4 : 2}
+                fill={
+                  highlightList.some(element => element === entry.id)
+                    ? '#F4BE37'
+                    : 'rgba(44,44,44,0.5)'
+                }
+              />
+            );
+          })}
+        </Area>
       </ComposedChart>
     </ResponsiveContainer>
   );
