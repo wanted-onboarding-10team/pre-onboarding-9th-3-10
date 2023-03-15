ac4eacf943b1ca16c8eb8e6f0d92f66d7ef5bf4a
yoosion030 <rsy5487@naver.com>
2023-03-15 11:19:22 +0900

    [#3] feature: YAxios label 추가
    Y축에 보여지는 값 정보 표시

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -32,13 +32,23 @@ const Chart = ({ data }: ChartProps) => {
     <ComposedChart width={1400} height={800} data={data}>
       <CartesianGrid strokeDasharray='3 3' />
       <XAxis dataKey='time' />
-      <YAxis dataKey='value_bar' tickCount={8} />
-      <YAxis dataKey='value_bar' tickCount={8} />
+      <YAxis
+        dataKey='value_area'
+        orientation='right'
+        yAxisId={0}
+        label={{ value: 'value_area', angle: 90, position: 'insideRight' }}
+      />
+      <YAxis
+        dataKey='value_bar'
+        orientation='left'
+        yAxisId={1}
+        label={{ value: 'value_bar', angle: -90, position: 'insideLeft' }}
+      />
       <Tooltip content={<CustomTooltip />} contentStyle={{ backgroundColor: '#fff' }} />
 
       <Legend />
 
-      <Bar type='monotone' dataKey='value_bar' fill='#82ca9d' />
+      <Bar type='monotone' dataKey='value_bar' fill='#82ca9d' yAxisId={1} />
       <Area type='monotone' dataKey='value_area' stroke='#8884d8' />
     </ComposedChart>
   ) : (
