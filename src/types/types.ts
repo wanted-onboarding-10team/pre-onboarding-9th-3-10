223aa348d8a5fcc8c6c93db17044414e852c697e
Shinpanda <duldulgo1@gmail.com>
2023-03-17 21:49:46 +0900

    [#39] refactor: 일자별 데이터 가공 추가 이후 확장성을 위하여 데이터를 일자별로 정리하는 기능을 추가 다른 일자가 들어오는 경우 selectedDay를 상태값으로 변경 및 select 박스로 변경하면 쉽게 사용 가능 [#39]

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,18 @@
 export interface ChartData {
   date: string;
+  time: string;
   id: string;
   value_area: number;
   value_bar: number;
+}
+export interface ChartDataByDay {
+  [key: string]: ChartData[];
+}
+
+export interface ChartDataResponse {
+  [date: string]: {
+    id: string;
+    value_area: number;
+    value_bar: number;
+  };
 }
